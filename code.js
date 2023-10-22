550a515f16c7a4613e771e642295999779278c58
purvi1802 <147663991+purvi1802@users.noreply.github.com>
2023-10-23 01:31:02 +0530

    code.js

--- code.js
+++ code.js
@@ -1,7 +1,11 @@
+// Import the Express.js framework
 const express = require('express');
+// Create an instance of the Express application
 const app = express();
 
+// Define a route that handles GET requests to '/get-quote'
 app.get('/get-quote', (req, res) => {
+    // An array of quotes
     const quotes = [
         "Success is not final; failure is not fatal: It is the courage to continue that counts.",
         "It is better to fail in originality than to succeed in imitation.",
@@ -11,8 +15,13 @@ app.get('/get-quote', (req, res) => {
         "Success is getting what you want, happiness is wanting what you get.",
         "There are three ways to ultimate success: The first way is to be kind. The second way is to be kind. The third way is to be kind."
     ];
+
+    // Pick a random quote from the array
     const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
-    res.json({ quote: randomQuote});
+    
+    // Respond to the request with a JSON object containing the random quote
+    res.json({ quote: randomQuote });
 })
 
-app.listen(3000, () => console.log('listening to port 3000..'));
+// Start the server, listening on port 3000, and log a message when it's ready
+app.listen(3000, () => console.log('Listening on port 3000..'));
